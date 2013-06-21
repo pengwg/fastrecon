7177d91390c2a6e5e19938557b25925f8dea3416
Peng Wang <pengwg@gmail.com>
2013-06-21 13:46:28 -0500

    Consolidate SOS functions

--- cpu/ImageRecon.h
+++ cpu/ImageRecon.h
@@ -8,7 +8,7 @@ class ImageRecon
 public:
     ImageRecon(ImageData &imageData, const ImageSize &reconSize);
     ImageData SOS() const;
-    ImageData SOS(ImageData &map) const;
+    ImageData SOS(const ImageData &map) const;
 
 private:
     ImageData m_imageData;
