fb3c09d683742dddc2d32104d79bd486b5a01781
Peng Wang <pengwg@gmail.com>
2014-07-13 22:33:47 -0500

    Initial creation of basicReconData extracted from ReconData

--- common/ReconData.cpp
+++ common/ReconData.cpp
@@ -3,7 +3,7 @@
 #include "ReconData.h"
 
 ReconData::ReconData(int size)
-    :m_size(size)
+    : basicReconData(size)
 {
 }
 
@@ -65,66 +65,6 @@ void ReconData::transformTrajComponent(float translation, float scale, int comp)
 
     m_bounds[comp].first = (m_bounds[comp].first + translation) * scale;
     m_bounds[comp].second = (m_bounds[comp].second + translation) * scale;
-}
-
-void ReconData::loadFromFiles(const QStringList &dataFileList, const QStringList &trajFileList, const QString &dcfFileName)
-{
-    std::cout << std::endl << "Read trajectory:" << std::endl;
-    for (const QString &name : trajFileList)
-    {
-        std::cout << name.toStdString() << std::endl;
-        FloatVector *traj = new FloatVector(m_size);
-
-        QFile file(name);
-        file.open(QIODevice::ReadOnly);
-        auto count = file.read((char *)traj->data(), m_size * sizeof(FloatVector::value_type));
-        file.close();
-
-        if (count != m_size * sizeof(FloatVector::value_type))
-        {
-            std::cout << "Error: wrong data size in " << name.toStdString() << std::endl;
-            std::exit(1);
-        }
-
-        addTrajComponent(traj);
-    }
-
-    std::cout << std::endl << "Read dcf:" << std::endl;
-    std::cout << dcfFileName.toStdString() << std::endl;
-    FloatVector *dcf = new FloatVector(m_size);
-
-    QFile file(dcfFileName);
-    file.open(QIODevice::ReadOnly);
-    auto count = file.read((char *)dcf->data(), m_size * sizeof(FloatVector::value_type));
-    file.close();
-
-    if (count != m_size * sizeof(FloatVector::value_type))
-    {
-        std::cout << "Error: wrong data size in " << dcfFileName.toStdString() << std::endl;
-        std::exit(1);
-    }
-    setDcf(dcf);
-
-    // Load data
-    std::cout << std::endl << "Read data:" << std::endl;
-    for (const QString &name : dataFileList)
-    {
-        ComplexVector *kdata = new ComplexVector(m_size);
-        std::cout << name.toStdString() << std::endl;
-
-        QFile file(name);
-        file.open(QIODevice::ReadOnly);
-        auto count = file.read((char *)kdata->data(), m_size * sizeof(ComplexVector::value_type));
-        file.close();
-
-        if (count != m_size * sizeof(ComplexVector::value_type))
-        {
-            std::cout << "Error: wrong data size in " << name.toStdString() << std::endl;
-            std::exit(1);
-        }
-
-        addChannelData(kdata);
-    }
 }
 
 void ReconData::clear()
