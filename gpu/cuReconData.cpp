00a42a7b922b681b4eeadc24c0078a9e52ed3922
Peng Wang <pengwg@gmail.com>
2014-07-13 23:12:51 -0500

    Initial creation of cuReconData class

---
+++ gpu/cuReconData.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+#include "cuReconData.h"
+
+cuReconData::cuReconData(int size)
+    : basicReconData(size)
+{
+}
+
+void cuReconData::addChannelData(const ComplexVector *data)
+{
+    if (m_size != data->size())
+    {
+        std::cerr << "Error: trajectory and data have different size!" << std::endl;
+        exit(1);
+    }
+
+    m_kDataMultiChannel.push_back(std::unique_ptr<const ComplexVector>(data));
+}
+
+void cuReconData::addTrajComponent(FloatVector *trajComp)
+{
+    if (m_size != trajComp->size())
+    {
+        std::cerr << "Error: data size does not match!" << std::endl;
+        exit(1);
+    }
+
+    if (rcDim() == 3)
+    {
+        std::cout << "3 trajectories have been loaded, ignoring additional data." << std::endl;
+        return;
+    }
+    m_traj.push_back(std::unique_ptr<FloatVector>(trajComp));
+
+    auto bound = std::minmax_element(trajComp->begin(), trajComp->end());
+    m_bounds.push_back(std::make_pair(*bound.first, *bound.second));
+
+    std::cout << "Range: " << '(' << *bound.first << ", " << *bound.second << ')' << std::endl;
+}
+
+void cuReconData::setDcf(FloatVector *dcf)
+{
+    if (m_size != dcf->size())
+    {
+        std::cerr << "Error: data size does not match!" << std::endl;
+        exit(1);
+    }
+
+    m_dcf.reset(dcf);
+}
+
+void cuReconData::transformTrajComponent(float translation, float scale, int comp)
+{
+    if (comp > rcDim())
+    {
+        std::cout << "Scale component not exists" << std::endl;
+        return;
+    }
+
+    for (auto &sample : *m_traj[comp])
+    {
+        sample = (sample + translation) * scale;
+    }
+
+    m_bounds[comp].first = (m_bounds[comp].first + translation) * scale;
+    m_bounds[comp].second = (m_bounds[comp].second + translation) * scale;
+}
+
+void cuReconData::clear()
+{
+    m_size = 0;
+
+    m_traj.clear();
+    m_dcf.reset();
+    m_kDataMultiChannel.clear();
+    m_bounds.clear();
+}
