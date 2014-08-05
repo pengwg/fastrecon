62ccdd933d1213e25a4792faa70610cf4471ecd9
Peng Wang <pengwg@gmail.com>
2014-08-05 17:09:34 -0500

    Experimantal nvcc c++11 support

--- gpu/basicReconData.cu
+++ gpu/basicReconData.cu
@@ -10,7 +10,7 @@ void basicReconData<T>::thrust_scale(thrust::device_vector<Point<T> > &traj, T t
 template<typename T>
 void basicReconData<T>::cuPreprocess(const thrust::device_vector<Point<T> > &traj, T half_W) const
 {
-    thrust::device_vector<int>  *cells_per_sample = new thrust::device_vector<int> (traj.size());
+    auto cells_per_sample = new thrust::device_vector<int> (traj.size());
     thrust::transform(traj.begin(), traj.end(), cells_per_sample->begin(), compute_num_cells_per_sample(half_W, m_dim));
     delete cells_per_sample;
 }
