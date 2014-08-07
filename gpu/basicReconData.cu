9b1d51077053d9dc2eb18c9268003dfda6ec717b
Peng Wang <pengwg@gmail.com>
2014-08-07 17:18:03 -0500

    Process chunk of data to avoid out of memory

--- gpu/basicReconData.cu
+++ gpu/basicReconData.cu
@@ -39,39 +39,39 @@ struct compute_num_cells_per_sample
 };
 
 template<typename T> __global__
-void write_pairs_kernel(const Point<T> *traj, unsigned *tuple_index, unsigned *tuples_first, unsigned *tuples_last, int reconSize, T half_W, int num_samples)
+void write_pairs_kernel(const Point<T> *traj, unsigned *tuple_index, int *tuples_first, int *tuples_last, int reconSize, T half_W, int num_samples)
 {
     unsigned sample_idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (sample_idx >= num_samples)
         return;
 
     const Point<T> &sample = traj[sample_idx];
-    unsigned lb[3], ub[3];
+    int lb[3], ub[3];
 
     for (int i = 0; i < 3; ++i)
     {
-        lb[i] = (unsigned)ceil(sample.x[i] - half_W);
-        ub[i] = (unsigned)floor(sample.x[i] + half_W);
+        lb[i] = (int)ceil(sample.x[i] - half_W);
+        ub[i] = (int)floor(sample.x[i] + half_W);
     }
 
     unsigned write_offset = sample_idx == 0 ? 0 : tuple_index[sample_idx - 1];
     int counter = 0;
 
-    if (sample_idx < 10)
-        printf("sample: %d, offset: %d, lb[0]: %d, ub[0]: %d\n", sample_idx, write_offset, lb[0], ub[0]);
+    //if (sample_idx < 10)
+        //printf("sample: %d, offset: %d, lb[0]: %d, ub[0]: %d\n", sample_idx, write_offset, lb[2], ub[2]);
 
-    /*for (unsigned z = lb[2]; z <= ub[2]; ++z)
+    for (int z = lb[2]; z <= ub[2]; ++z)
+    {
+        for (int y = lb[1]; y <= ub[1]; ++y)
         {
-            for (unsigned y = lb[1]; z <= ub[1]; ++y)
+            for (int x = lb[0]; x <= ub[0]; ++x)
             {
-                for (unsigned x = lb[0]; x <= ub[0]; ++x)
-                {
-                    //tuples_first[write_offset + counter] = x + y * reconSize + z * reconSize * reconSize;
-                    //tuples_last[write_offset + counter] = sample_idx;
-                    ++counter;
-                }
+                tuples_first[write_offset + counter] = x + y * reconSize + z * reconSize * reconSize;
+                tuples_last[write_offset + counter] = sample_idx;
+                ++counter;
             }
-        }*/
+        }
+    }
 
 }
 
@@ -91,23 +91,24 @@ void basicReconData<T>::cuPreprocess(const thrust::device_vector<Point<T> > &tra
     thrust::inclusive_scan(cells_per_sample->begin(), cells_per_sample->end(), tuple_index->begin(), thrust::plus<unsigned> ());
     delete cells_per_sample;
 
-    unsigned num_of_pairs = tuple_index->back();
-    std::cout << " Traj size: " << traj.size() << " Number of pairs: " << num_of_pairs << std::endl;
+    std::cout << " Traj size: " << traj.size() << " Number of pairs: " << tuple_index->back() << std::endl;
 
-    /*auto tuples_first = new thrust::device_vector<unsigned> (num_of_pairs);
-    auto tuples_last = new thrust::device_vector<unsigned> (num_of_pairs);
+    unsigned chunk_size = 524288;
+    unsigned num_of_pairs = tuple_index->data()[chunk_size];
+    auto tuples_first = new thrust::device_vector<int> (num_of_pairs);
+    auto tuples_last = new thrust::device_vector<int> (num_of_pairs);
 
     const Point<T> *traj_ptr = thrust::raw_pointer_cast(traj.data());
     unsigned *tuple_index_ptr = thrust::raw_pointer_cast(tuple_index->data());
-    unsigned *tuples_first_ptr = thrust::raw_pointer_cast(tuples_first->data());
-    unsigned *tuples_last_ptr = thrust::raw_pointer_cast(tuples_last->data());
+    int *tuples_first_ptr = thrust::raw_pointer_cast(tuples_first->data());
+    int *tuples_last_ptr = thrust::raw_pointer_cast(tuples_last->data());
 
     int blockSize = 256;
-    int gridSize = (int)ceil((double)traj.size() / blockSize);
-    write_pairs_kernel<T><<<gridSize, blockSize>>>(traj_ptr, tuple_index_ptr, tuples_first_ptr, tuples_last_ptr, reconSize, half_W, traj.size());
+    int gridSize = (int)ceil((double)chunk_size / blockSize);
+    write_pairs_kernel<T><<<gridSize, blockSize>>>(traj_ptr, tuple_index_ptr, tuples_first_ptr, tuples_last_ptr, reconSize, half_W, chunk_size);
 
     delete tuples_first;
-    delete tuples_last; */
+    delete tuples_last;
     delete tuple_index;
 }
 
