0aa1f6b266a63c0cbf46c94166f971fc6496dd97
Peng Wang <pengwg@gmail.com>
2014-08-15 15:22:52 -0500

    Clean up types definition

--- ImageData/common.h
+++ ImageData/common.h
@@ -9,50 +9,27 @@
 template<typename T>
 using ComplexVector = std::vector<std::complex<T>>;
 
-template<template<typename, typename> class C, typename T>
-struct LocalVectorType {
-    typedef std::vector<T> type;
-};
+template<typename T>
+using cuVector = typename thrust::device_vector<T>;
 
 template<typename T>
-struct LocalVectorType<thrust::device_vector, T> {
-    typedef thrust::device_vector<T> type;
-};
-
-template<typename T>
-struct LocalVectorType<thrust::host_vector, T> {
-    typedef thrust::host_vector<T> type;
-};
-
-template<template<typename, typename> class C, typename T>
-struct LocalComplexVectorType {
-    typedef ComplexVector<T> type;
+struct cuComplexVectorType
+{
 };
 
 template<>
-struct LocalComplexVectorType<thrust::device_vector, float> {
+struct cuComplexVectorType<float>
+{
     typedef thrust::device_vector<cuComplex> type;
 };
 
 template<>
-struct LocalComplexVectorType<thrust::device_vector, double> {
+struct cuComplexVectorType<double>
+{
     typedef thrust::device_vector<cuDoubleComplex> type;
 };
 
-template<>
-struct LocalComplexVectorType<thrust::host_vector, float> {
-    typedef thrust::host_vector<cuComplex> type;
-};
-
-template<>
-struct LocalComplexVectorType<thrust::host_vector, double> {
-    typedef thrust::host_vector<cuDoubleComplex> type;
-};
-
 template<typename T>
-using cuVector = typename LocalVectorType<thrust::device_vector, T>::type;
-
-template<typename T>
-using cuComplexVector = typename LocalComplexVectorType<thrust::device_vector, T>::type;
+using cuComplexVector = typename cuComplexVectorType<T>::type;
 
 #endif // COMMON_H
